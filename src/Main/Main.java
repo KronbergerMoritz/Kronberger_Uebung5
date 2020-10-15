5ebe1260dafe4a762ac13759aa6d950f524af288
Moritz Kronberger <moritzkronberger@aon.at>
2020-10-15 10:54:06 +0200

    Initiated Project

---
+++ src/Main/Main.java
@@ -0,0 +1,22 @@
+package Main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Author: Moritz Kronberger
+ * Date: 15.10.2020
+ */
+public class Main extends Application
+{
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+
+    }
+}
